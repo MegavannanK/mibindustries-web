3e2a18fae084982ae234b4ba222afacd9e9df19b
Nandy Crescentic <nandy@crescentic.io>
2025-01-08 21:12:23 +0530

    chore : responsive ness wrt to 90%

--- app/components/Homepage/Experience.tsx
+++ app/components/Homepage/Experience.tsx
@@ -23,29 +23,33 @@ const stats = [
 
 export const Experience = () => {
   return (
-    <div className="bg-primary-800 px-4 py-6 h-[30vh] md:h-[50vh] overflow-hidden">
+    <div className="bg-primary-800 px-4 py-6 h-[45vh] md:h-[50vh] overflow-hidden">
       <div className="relative">
         <img
-          className="absolute md:-top-[160px] opacity-50"
+          className="md:w-full absolute md:-top-[160px] opacity-50"
           src={experienceHeroImage.src}
           alt="Hero Background"
         />
-        <div className="flex flex-row gap-10 w-1/2 mx-auto mt-20">
+        <div
+          className={`relative grid grid-cols-2 ${
+            stats.length > 1 ? "md:grid-cols-3" : "justify-center"
+          } gap-10 md:w-1/2 mx-auto mt-5 md:mt-20 items-center`}
+        >
           {stats.map((stat, index) => (
             <div
               key={index}
-              className={`bg-white rounded-lg drop-shadow-md p-4 flex flex-col gap-4 w-[300px] h-56 items-center justify-between ${
-                stat.label === "Happy Users" ? "mt-16" : ""
+              className={`bg-white rounded-lg shadow-[0px_4px_10px_rgba(255,255,255,0.3)] p-4 flex flex-col gap-4 md:w-[300px] md:h-56 items-center justify-between ${
+                stat.label === "Happy Users" ? "md:mt-16" : ""
               }`}
             >
-              <div className="bg-primary-700 h-20 w-20 flex items-center justify-center rounded-md">
+              <div className="bg-primary-700 h-10 w-10 md:h-20 md:w-20 flex items-center justify-center rounded-md">
                 <img src={stat.image.src} alt={stat.label} className="w-12" />
               </div>
               <div className="text-center">
-                <h2 className="text-primary-900 text-title-7 font-semibold">
+                <h2 className="text-primary-900 text-title-9 md:text-title-7 font-semibold">
                   {stat.count}
                 </h2>
-                <p className="text-title-9 text-primary-500 font-medium">
+                <p className="text-body-3 md:text-title-9 text-primary-500 font-medium">
                   {stat.label}
                 </p>
               </div>
