bc9c9a0d404f206f56ecc581cecd78d1c8d8a4a2
Mathesh24 <mbabu003@ucr.edu>
2025-08-05 09:01:20 -0400

    Image change for sucess story

--- app/masters/sucess_story.ts
+++ app/masters/sucess_story.ts
@@ -1,4 +1,6 @@
 export const success_story = [
-    "https://res.cloudinary.com/dd7bw6igp/image/upload/v1746476982/MIB-website/Homepage/success_story_blue_elephant/ztwnstmpujvgsn5odlta.jpg",
-    "https://res.cloudinary.com/dd7bw6igp/image/upload/v1746477042/MIB-website/Homepage/success_story_blue_elephant/shxq1el6u53wwwsmjpjv.jpg"
+    // "https://res.cloudinary.com/dd7bw6igp/image/upload/v1746476982/MIB-website/Homepage/success_story_blue_elephant/ztwnstmpujvgsn5odlta.jpg",
+    // "https://res.cloudinary.com/dd7bw6igp/image/upload/v1746477042/MIB-website/Homepage/success_story_blue_elephant/shxq1el6u53wwwsmjpjv.jpg"
+    // "https://res.cloudinary.com/dd7bw6igp/image/upload/v1754219179/blue_elephant_nbjv5u.png",
+    "https://res.cloudinary.com/dd7bw6igp/image/upload/v1754386516/Blue_elephant_banner_aaotcy.png"
 ];
