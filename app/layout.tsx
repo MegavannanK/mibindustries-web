1321d85d3eb016b6c74d8bc28a5e06a1a2f50a2e
Megavannan Kandasamy <megavannan@Megavannans-MacBook-Air.local>
2026-02-28 11:15:45 +0530

    Bug Fix

--- app/layout.tsx
+++ app/layout.tsx
@@ -66,10 +66,18 @@ export default function RootLayout({
     <html lang="en-IN">
       <head>
         {/* Google Tag Manager */}
-        <Script
-          src="https://www.googletagmanager.com/gtm.js?id=GTM-PCFV7PH7"
-          strategy="afterInteractive"
-        />
+        <Script id="gtm-script" strategy="afterInteractive">
+        {`
+        (function(w,d,s,l,i){w[l]=w[l]||[];
+        w[l].push({'gtm.start': new Date().getTime(),event:'gtm.js'});
+        var f=d.getElementsByTagName(s)[0],
+        j=d.createElement(s),dl=l!='dataLayer'?'&l='+l:'';
+        j.async=true;
+        j.src='https://www.googletagmanager.com/gtm.js?id='+i+dl;
+        f.parentNode.insertBefore(j,f);
+        })(window,document,'script','dataLayer','GTM-PCFV7PH7');
+        `}
+        </Script>
 
         {/* Existing Meta */}
         <link rel="icon" href="/assets/images/logo.png" type="image/png" />
@@ -112,6 +120,14 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
+        <noscript>
+          <iframe
+            src="https://www.googletagmanager.com/ns.html?id=GTM-PCFV7PH7"
+            height="0"
+            width="0"
+            style={{ display: "none", visibility: "hidden" }}
+          />
+        </noscript>
         {children}
       </body>
     </html>
